63e73eb5c373200e867befd55ff8fb187421d90b
Jiaxing Hu <jiaxinghu52@gmail.com>
2020-09-25 15:05:40 +0200

    New backup 2020-09-25 15:05:40

---
+++ backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if git is avaible
+IS_GIT_AVAILABLE="$(git --version)"
+if [[ $IS_GIT_AVAILABLE == *"version"* ]]; then
+  echo "Git is Available"
+else
+  echo "Git is not installed"
+  exit 1
+fi
+
+# Copy dotfiles
+cp  $HOME/{.zshrc,.vimrc} .
+
+gs="$(git status | grep -i "modified")"
+
+# Check if dotfiles are modified
+if [[ $gs == *"modified"* ]]; then
+  echo "push"
+fi
+
+# push to Github
+git add -u;
+git commit -m "New backup `date +'%Y-%m-%d %H:%M:%S'`";
+git push origin master
