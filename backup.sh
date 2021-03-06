aba519122a2d5dae207413151392aaaca5809ebc
Jiaxing Hu <jiaxinghu52@gmail.com>
2021-03-06 15:31:10 +0100

    New backup 2021-03-06 15:31:10

--- backup.sh
+++ backup.sh
@@ -9,21 +9,9 @@ else
     exit 1
 fi
 
-# Copy dotfiles
-cp $HOME/{.zshrc,.aliases,.Xresources} .
-
-if [ ! -d "./.config" ]; then
-    mkdir .config
-fi
-
-rsync -r -l $HOME/.config/{i3,bspwm,sxhkd,picom,polybar,qt5ct,alacritty,kitty} ./.config
-rsync $HOME/.config/nvim/{init.vim,config.vim,coc-config.vim,keybindings.vim,plugins.vim} ./.config/nvim
-rsync -r $HOME/.config/nvim/lua/* ./.config/nvim/lua
-rsync -r $HOME/Pictures/Wallpapers/* Pictures/Wallpapers
+gs="$(git status | grep -i "modified")"
 
 git add -A
-
-gs="$(git status | grep -i "modified")"
 
 # Check if dotfiles are modified
 if [[ $gs == *"modified"* ]]; then
